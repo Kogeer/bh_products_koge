13ef94aaf2c45132f0988d9ca28c1df2f4900e22
Gergely Kovacs <gergely.kovacs02@gmail.com>
2020-03-05 17:48:52 +0100

    Only-choose-group-from-database

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('inventory.db');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('inventory.db');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -17,25 +17,32 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    db.serialize(function() {
-        db.all("SELECT * from products", function(err, results) {
+    db.serialize(function () {
+        db.all("SELECT * from products", function (err, results) {
             if (err != null) {
                 console.log(`Something went wrong: ${err.toString()}`);
             }
-          res.render('home', {
-              pageTitle: 'Products',
-              products: results
-          })
+            db.all("SELECT * from inventory_groups", function(err,groups) {
+                if(err != null) {
+                    console.log(`Something went wrong: ${err.toString()}`);
+                }
+                res.render('home', {
+                    pageTitle: 'Products',
+                    products: results,
+                    groups: groups
+                })
+            })
             
+
         });
-      });
+    });
 
 });
 
-app.post('/addproduct', (req,res) => {
-    const { newproduct, newgroup,productDescribe } = req.body;
+app.post('/addproduct', (req, res) => {
+    const { newproduct, newgroup, productDescribe } = req.body;
     db.serialize(function () {
-        db.run("INSERT INTO products(name,category,describe) VALUES (?, ?, ?)", [newproduct,newgroup,productDescribe]);
+        db.run("INSERT INTO products(name,category,describe) VALUES (?, ?, ?)", [newproduct, newgroup, productDescribe]);
 
         db.get(`SELECT id FROM products WHERE name = "${newproduct}" AND category = "${newgroup}"`, (err, result) => {
             if (err != null) {
@@ -52,66 +59,66 @@ app.post('/addproduct', (req,res) => {
     });
 });
 
-app.get('/inventory',(req,res) => {
+app.get('/inventory', (req, res) => {
     db.serialize(function () {
         db.all("SELECT products.id, products.name, products.category, inventory.stock FROM products JOIN inventory ON products.id = inventory.product_id", function (err, results) {
             if (err != null) {
                 console.error(err.toString())
             }
-            res.render('inventory',{pageTitle: 'Inventory', invent: results});
+            res.render('inventory', { pageTitle: 'Inventory', invent: results });
         });
     });
 });
 
-app.post('/editinv',(req,res) => {
-    const {newpieces,id} = req.body;
-    db.serialize(function() {
+app.post('/editinv', (req, res) => {
+    const { newpieces, id } = req.body;
+    db.serialize(function () {
         db.run(`UPDATE inventory SET stock = "${newpieces}" WHERE product_id = "${id}"`);
     });
 
     res.redirect('/inventory');
 });
 
-app.post('/editproduct',(req,res) => {
-    const {newproduct,newgroup,id,productDescribe} = req.body;
-    db.serialize(function() {
+app.post('/editproduct', (req, res) => {
+    const { newproduct, newgroup, id, productDescribe } = req.body;
+    db.serialize(function () {
         db.run(`UPDATE products SET name = "${newproduct}", category = "${newgroup}", describe = "${productDescribe}" WHERE products.id = "${id}"`);
     });
     res.redirect('/');
 });
 
-app.post('/deleteproduct',(req,res) => {
-    const {id} = req.body;
-    db.serialize(function() {
+app.post('/deleteproduct', (req, res) => {
+    const { id } = req.body;
+    db.serialize(function () {
         db.run(`DELETE FROM products WHERE products.id="${id}"`);
         db.run(`DELETE FROM inventory WHERE product_id="${id}"`);
     })
     res.redirect('/');
 });
 
-app.get('/groups',(req,res) => {
-    db.serialize(function() {
-        db.all("SELECT name FROM inventory_groups", function(err, results) {
+app.get('/groups', (req, res) => {
+    db.serialize(function () {
+        db.all("SELECT name FROM inventory_groups", function (err, results) {
             if (err != null) {
                 console.log(`Something went wrong: ${err.toString()}`);
             }
-          res.render('groups', {
-              pageTitle: 'Groups',
-              groupElements: results
-          })
-            
+            res.render('groups', {
+                pageTitle: 'Groups',
+                groupElements: results
+            })
+
         });
-      });
+    });
 });
 
-app.post('/addgroup',(req,res) => {
+app.post('/addgroup', (req, res) => {
     const { newgroup } = req.body;
     console.log(newgroup);
     db.serialize(function () {
         db.run("INSERT INTO inventory_groups(name) VALUES (?)", [newgroup]);
         res.redirect('groups');
     });
-})
+});
 
 app.listen(port, () => {
     console.log(`This app listen on port: ${port}`);
